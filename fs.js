cd4fc5795a7f5f6dc1e30c0fe199d1c67dc13f31
heapwolf <paolo@async.ly>
2022-04-25 21:11:43 +0200

    tidy

--- fs.js
+++ fs.js
@@ -1,10 +1,16 @@
 'use strict'
 const EventEmitter = require('./events')
 
+const rand64 = () => BigInt(
+  ((Math.random() * Number.MAX_SAFE_INTEGER) >>> 0).toString(2).repeat(2)
+)
+
 class FileHandle extends EventEmitter {
-  constructor (fd) {
+  constructor (id) {
     super()
-    this.fd = fd
+    // this id will be used to identify the file handle that is a reference
+    // stored in a map container on the objective-c side of the bridge.
+    this.id = id
   }
 
   async close (cb) {
@@ -15,13 +21,23 @@ class FileHandle extends EventEmitter {
   }
 }
 
-const open = async (...args) => {
-  const id = BigInt(Number.MAX_SAFE_INTEGER)
-  const { err } = await window._ipc.send('fsOpen', args)
+const write = async () => {
+
+}
+
+const open = async (path, flags, mode) => {
+  const id = rand64()
+
+  // this id will be the key in the map that stores the file handle on the
+  // objective-c side of the bridge.
+  const { err } = await winow._ipc.send('fsOpen', { id, path, flags, mode })
+
   if (err) throw err
+
   return new FileHandle(id)
 }
 
 module.exports = {
-  open
+  open,
+  write
 }
