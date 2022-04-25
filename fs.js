bb1535f8c912e2f40e98dc8f67f08541c1262109
heapwolf <paolo@async.ly>
2022-04-25 11:00:46 +0200

    add fs.open

--- fs.js
+++ fs.js
@@ -1,4 +1,27 @@
 'use strict'
+const EventEmitter = require('./events')
+
+class FileHandle extends EventEmitter {
+  constructor (fd) {
+    super()
+    this.fd = fd
+  }
+
+  async close (cb) {
+    const { err } = await winow._ipc.send('fsClose', { id: this.fd })
+    if (err) throw err
+
+    cb()
+  }
+}
+
+const open = async (...args) => {
+  const id = BigInt(Number.MAX_SAFE_INTEGER)
+  const { err } = await winow._ipc.send('fsOpen', args)
+  if (err) throw err
+  return new FileHandle(id)
+}
 
 module.exports = {
+  open
 }
