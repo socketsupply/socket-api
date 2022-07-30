94b6c9ecb78883e3fc86c74bc5ff0aa6b94e5702
Joseph Werle <joseph.werle@gmail.com>
2022-07-30 16:35:56 +0200

    chore(): clean up

--- test/e2e/build.js
+++ test/e2e/build.js
@@ -2,28 +2,7 @@
 import path from 'node:path'
 import fs from 'node:fs/promises'
 
-import Minifier from 'clean-css'
 import esbuild from 'esbuild'
-
-const minifier = new Minifier({ advanced: true })
-
-const css = async (src, dest) => {
-  let str = await fs.readFile(src, 'utf8')
-  let RE = /@import ['"]([^'" ]+)['"];/g
-  let reqs = []
-
-  str.replace(RE, (_, p) => {
-    reqs.push(css(path.resolve(path.dirname(src), p)))
-  })
-
-  const data = await Promise.all(reqs)
-  str = str.replace(RE, () => data.shift())
-
-  const min = minifier.minify(str)
-
-  if (!dest) return min.styles
-  return fs.writeFile(dest, min.styles)
-}
 
 const cp = async (a, b) => fs.cp(
   path.resolve(a),
@@ -32,11 +11,6 @@ const cp = async (a, b) => fs.cp(
 )
 
 async function copy (target) {
-  await css(
-    path.join('src', 'index.css'),
-    path.join(target, 'index.css')
-  )
-
   await cp('src/index.html', target)
 }
 
@@ -47,34 +21,10 @@ async function main () {
     bundle: true,
     keepNames: true,
     platform: 'browser',
+    outdir: path.resolve(process.argv[2])
   }
 
-  //
-  // During development, this script will be started by npm and it
-  // will be passed the target directory for the build artifacts.
-  //
-  // During a build, this script will be called by the `op` command.
-  // In this case the target directory for the build artifacts will
-  // be provided as the argument at index 2.
-  //
-  let target = process.argv.find(s => s.includes('--watch='))
-
-  if (target) {
-    target = path.resolve(target.split('=')[1])
-    esbuild.serve({ servedir: target }, params)
-  } else {
-    params.outdir = path.resolve(process.argv[2])
-    //await esbuild.build({ ...params, minify: true })
-    await esbuild.build({ ...params })
-  }
-
-  console.log(params)
-
-  if (!params.outdir) {
-    console.log('Did not receive the build target path as an argument!')
-    process.exit(1)
-  }
-
+  await esbuild.build({ ...params })
   copy(params.outdir)
 }
 
