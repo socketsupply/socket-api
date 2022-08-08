f8a29f0d38597ba60d5bea1789e8d5d042957e69
Sergey Rubanov <chi187@gmail.com>
2022-08-08 15:49:32 +0400

    add crypto methods return values

--- crypto.js
+++ crypto.js
@@ -9,6 +9,7 @@ import { Buffer } from 'buffer'
 
 /*
  * @param {number} size - The number of bytes to generate. The size must not be larger than 2**31 - 1.
+ * @returns {Promise<Buffer>} - A promise that resolves with an instance of io.Buffer with random bytes.
  */
 export function randomBytes (size) {
   const tmp = new Uint8Array(size)
@@ -19,6 +20,7 @@ export function randomBytes (size) {
 /*
  * @param {string} algorithm - `SHA-1` | `SHA-256` | `SHA-384` | `SHA-512`
  * @param {Buffer} message - An instance of io.Buffer
+ * @returns {Promise<Buffer>} - A promise that resolves with an instance of io.Buffer with the hash.
  */
 export async function createDigest (algorithm, buf) {
   return Buffer.from(await crypto.subtle.digest(algorithm, buf))
