db2775ce721d1fcf0d0d5205a36e3f8eb3976928
Sergey Rubanov <chi187@gmail.com>
2022-08-12 20:10:40 +0400

    fix lookup test

--- test/src/dns.js
+++ test/src/dns.js
@@ -2,9 +2,12 @@ import { test } from 'tapzero'
 import { lookup } from '../../dns.js'
 
 test('dns', async t => {
-  t.ok(lookup, 'lookup is available')
-  lookup('sockets.sh', (err, address) => {
-    if (err) return t.fail(err)
-    t.ok(typeof address == 'string', 'lookup returns an address')
+  await new Promise (resolve => {
+    lookup('sockets.sh', (err, address) => {
+      if (err) return t.fail(err)
+      console.log(JSON.stringify(address))
+      t.ok(address.ip.length > 0, 'lookup returns an address')
+      resolve()
+    })
   })
 })
