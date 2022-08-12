1d589b4fd57aa1118efd9fff4c5e4cc7166266f7
heapwolf <paolo@socketsupply.co>
2022-08-12 16:41:31 +0200

    add test

--- test/src/dgram.js
+++ test/src/dgram.js
@@ -1,13 +1,34 @@
-import * as dgram from '@socketsupply/io/dgram'
+import * as io from '@socketsupply/io'
 import { test } from 'tapzero'
 
-test('udp kitchen client-server', async t => {
-  const server = dgram.createSocket({
+test('udp bind, send', async t => {
+  const server = io.dgram.createSocket({
     type: 'udp4',
     reuseAddr: false
   })
 
-  const client = dgram.createSocket('udp4')
+  const client = io.dgram.createSocket('udp4')
+
+  const msg = new Promise((resolve, reject) => {
+    server.on('message', resolve)
+    server.on('error', reject)
+  })
+
+  server.on('listening', () => {
+    client.send(io.Buffer.from('xxx'), 41234)
+  })
+
+  t.ok(server.bind(41234) === server)
+  t.ok((await msg).toString() === 'xxx')
+})
+
+test('udp bind, connect, send', async t => {
+  const server = io.dgram.createSocket({
+    type: 'udp4',
+    reuseAddr: false
+  })
+
+  const client = io.dgram.createSocket('udp4')
 
   const msg = new Promise((resolve, reject) => {
     server.on('message', resolve)
@@ -17,9 +38,10 @@ test('udp kitchen client-server', async t => {
   server.on('listening', () => {
     client.connect(41234, '0.0.0.0', (err) => {
       if (err) return t.fail(err.message)
-      client.send(Buffer.from('xxx'))
+      client.send(io.Buffer.from('xxx'))
     })
   })
+
   t.ok(server.bind(41234) === server)
   t.ok((await msg).toString() === 'xxx')
 })
