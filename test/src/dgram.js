8a247c4c1e7d2d34e8e3593d823464ab15948329
Dominic Tarr <dominic.tarr@gmail.com>
2022-08-12 19:04:46 +1200

    tests needed for p2p

---
+++ test/src/dgram.js
@@ -0,0 +1,52 @@
+
+import * as dgram from '@socketsupply/io/dgram'
+//import dgram from 'dgram' //uncomment to tests the tests, should pass running node
+import { test } from 'tapzero'
+
+test('bind a dgram port and send a packet to it', function (t) {
+  var resolve
+  var socket = dgram.createSocket('udp4'), socket2
+//  var p = ~~(Math.random()*0xffff)
+  socket.on('message', function (b) {
+    t.equal(b.toString(), 'hello!')
+    socket.close(function () {})
+    resolve()
+  })
+  t.equal(socket.bind(), socket, 'bind returns this') //any port
+
+  socket.on('listening', function () {
+    var addr = socket.address()
+    var p = addr.port
+    t.equal(addr.address, '0.0.0.0')
+    t.ok(Number.isInteger(addr.port))
+    socket.send(Buffer.from('hello!'), p, '127.0.0.1') //any port
+  })
+  return new Promise(_resolve => resolve = _resolve)
+})
+
+test('error if port is already bound', function (t) {
+  var resolve
+  var socket = dgram.createSocket('udp4')
+  var socket2 = dgram.createSocket('udp4')
+  var received = []
+  socket.on('message', function (b) {
+    console.log('receive:', b)
+    socket.close(function () {})
+    resolve()
+  })
+  t.equal(socket.bind(), socket, 'bind returns this') //any port
+
+  socket.on('listening', function () {
+    var addr = socket.address()
+    var p = addr.port
+    t.equal(addr.address, '0.0.0.0')
+    t.ok(Number.isInteger(addr.port))
+    socket2.on('error', function (err) {
+      t.equal(err.code, 'EADDRINUSE', 'address in use err')
+      socket.close()
+      resolve()
+    })
+    t.equal(socket2.bind(p), socket2)
+  })
+  return new Promise(_resolve => resolve = _resolve)
+})
--- test/src/os.js
+++ test/src/os.js
@@ -1,4 +1,5 @@
 import * as os from '@socketsupply/io/os'
+//import os from 'os' //uncomment to tests the tests, should pass running node
 import { test } from 'tapzero'
 
 test('os.arch()', (t) => {
@@ -14,6 +15,26 @@ test('os.type()', (t) => {
 })
 
 test('os.networkInterfaces()', (t) => {
+  var int = os.networkInterfaces()
+  function isAddress (i) {
+    console.log(i)
+    if(i.family === 4) {
+      t.ok(/\d+\.\d+\.\d+\.\d+/.test(i.address))
+    }
+      else t.equal(i.family, 6)
+
+    t.ok(i.netmask, 'has netmask')
+    t.ok(i.mac, 'has mac address')
+    t.equal(typeof i.internal, 'boolean')
+    t.ok(i.cidr, 'has cidr')
+  }
+
+  t.ok(Array.isArray(int.lo))
+  var interfaces = Object.keys(int).length
+  t.ok(interfaces >= 2, 'network interfaces has at least two keys, loopback + wifi, was:'+interfaces)
+  for(var intf in int) {
+    int[intf].forEach(isAddress)
+  }  
 })
 
 test('os.EOL', (t) => {
