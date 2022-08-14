7c6df4a3007e0de334b6fa6725ca2c7ce7f134d8
heapwolf <paolo@socketsupply.co>
2022-08-14 12:16:29 +0200

    not all hardware is going to have lo, or even lo0 interfaces, mac is only for internal

--- test/src/os.js
+++ test/src/os.js
@@ -21,24 +21,25 @@ test('os.type()', (t) => {
 test('os.networkInterfaces()', (t) => {
   var int = os.networkInterfaces()
   function isAddress (i) {
-    console.log(i)
-    if(i.family === 4) {
-      t.ok(/\d+\.\d+\.\d+\.\d+/.test(i.address))
+    if(i.family === 4 || i.family === 'IPv4') {
+      t.ok(/\d+\.\d+\.\d+\.\d+/.test(i.address), 'matches the format')
+    } else {
+      t.ok(i.family === 6 || i.family === 'IPv6')
     }
-      else t.equal(i.family, 6)
 
     t.ok(i.netmask, 'has netmask')
-    t.ok(i.mac, 'has mac address')
+    if (i.internal) t.ok(i.mac, 'has mac address')
     t.equal(typeof i.internal, 'boolean')
     t.ok(i.cidr, 'has cidr')
   }
 
-  t.ok(Array.isArray(int.lo))
+
+  t.ok(Array.isArray(int.lo) || Array.isArray(int.lo0), 'iterface is "lo"')
   var interfaces = Object.keys(int).length
   t.ok(interfaces >= 2, 'network interfaces has at least two keys, loopback + wifi, was:'+interfaces)
-  for(var intf in int) {
+  for (const intf in int) {
     int[intf].forEach(isAddress)
-  }  
+  }
 
   const networkInterfaces = os.networkInterfaces()
   t.ok(networkInterfaces, 'os.networkInterfaces()')
