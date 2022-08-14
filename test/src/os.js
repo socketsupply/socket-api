596a30b6a29d3677f9f2b54b6cc93bf3d8383b3d
Dominic Tarr <dominic.tarr@gmail.com>
2022-08-14 22:08:04 +1200

    tests needed for p2p (currently failing) (#23)
    tests needed for p2p
    
    Co-authored-by: heapwolf <paolo@async.ly>

--- test/src/os.js
+++ test/src/os.js
@@ -1,4 +1,5 @@
 import * as os from '@socketsupply/io/os'
+//import os from 'os' //uncomment to tests the tests, should pass running node
 import { test } from 'tapzero'
 
 const archs = ['arm64', 'ia32', 'x64',  'unknown']
@@ -18,6 +19,27 @@ test('os.type()', (t) => {
 })
 
 test('os.networkInterfaces()', (t) => {
+  var int = os.networkInterfaces()
+  function isAddress (i) {
+    console.log(i)
+    if(i.family === 4) {
+      t.ok(/\d+\.\d+\.\d+\.\d+/.test(i.address))
+    }
+      else t.equal(i.family, 6)
+
+    t.ok(i.netmask, 'has netmask')
+    t.ok(i.mac, 'has mac address')
+    t.equal(typeof i.internal, 'boolean')
+    t.ok(i.cidr, 'has cidr')
+  }
+
+  t.ok(Array.isArray(int.lo))
+  var interfaces = Object.keys(int).length
+  t.ok(interfaces >= 2, 'network interfaces has at least two keys, loopback + wifi, was:'+interfaces)
+  for(var intf in int) {
+    int[intf].forEach(isAddress)
+  }  
+
   const networkInterfaces = os.networkInterfaces()
   t.ok(networkInterfaces, 'os.networkInterfaces()')
   t.ok(Object.keys(networkInterfaces).length > 0, 'os.networkInterfaces() not empty')
