9eda0cf7f14ed25923ab891394ed93d224acbe35
heapwolf <paolo@voltra.co>
2022-08-15 15:24:42 +0200

    fix dns test

--- test/src/dns.js
+++ test/src/dns.js
@@ -2,14 +2,23 @@ import { test } from 'tapzero'
 import { lookup } from '../../dns.js'
 
 const IPV4_REGEX = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+const IPV6_REGEX = /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/
+
 test('dns', async t => {
   if (globalThis?.navigator?.onLine) {
     await new Promise (resolve => {
       lookup('sockets.sh', (err, info) => {
         if (err) return t.fail(err)
-        t.ok(info && typeof info === 'object')
-        t.equal(4, info.family, 'is IPv4 family')
-        t.ok(IPV4_REGEX.test(info.address), 'is IPv4 address')
+        t.ok(info && typeof info === 'object', 'is a valid object')
+
+        const isValidFamily = (info.family === 4) || (info.family === 6)
+
+        t.ok(isValidFamily, 'is either IPv4 or IPv6 family')
+
+        const v4 = IPV4_REGEX.test(info.address)
+        const v6 = IPV6_REGEX.test(info.address)
+
+        t.ok(v4 || v6, 'is valid address')
         resolve()
       })
     })
