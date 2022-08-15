eb77efcf3e1b917354e2534eb4a4f2003eec4438
Sergey Rubanov <chi187@gmail.com>
2022-08-15 16:03:20 +0400

    fix process.cwd test

--- test/src/process.js
+++ test/src/process.js
@@ -16,7 +16,7 @@ test('process.exit()', (t) => {
 
 test('process.cwd', (t) => {
   t.ok(typeof process.cwd() === 'string', 'process.cwd() returns a string')
-  if (process.platform  === 'win32') {
+  if (process.platform  !== 'win32') {
     t.equal(process.cwd(), path.resolve(process.argv0, '../../Resources'), 'process.cwd() returns a correct value')
   }
 })
