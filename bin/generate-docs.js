d8d6c4b41a27747229bd6d04dc32f6defe0c136e
Sergey Rubanov <chi187@gmail.com>
2022-09-07 20:41:31 +0400

    (docs) improve @return parser

--- bin/generate-docs.js
+++ bin/generate-docs.js
@@ -164,7 +164,7 @@ export function transform (filename) {
         if (isReturn) {
           const propType = 'returns'
           const { 1: type, 2: rawName } = attr.match(/{([^}]+)}(?:\s*-\s*)?(.*)/)
-          const [name, description] = rawName.split(/\s-\s/s)
+          const [name, description] = rawName.split(/-\s+/)
           const param = { name: name.trim() || 'Not specified' , type, description: description?.trim() }
           if (['undefined', 'void'].includes(type)) continue;
           if (!item[propType]) item[propType] = []
