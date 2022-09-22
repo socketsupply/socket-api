62353534e24b402fad3fb69e9241c139c2e67849
Sergey Rubanov <chi187@gmail.com>
2022-09-22 18:14:05 +0400

    optimizations wip

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,73 @@
 import { readFile, writeFile } from './fs/promises.js'
-import { createWriteStream } from './fs/index.js'
 import { createDigest } from './crypto.js'
 import { EventEmitter } from './events.js'
+
+async function* streamAsyncIterable(stream) {
+  const reader = stream.getReader();
+  try {
+    while (true) {
+      const { done, value } = await reader.read();
+      if (done) return;
+      yield value;
+    }
+  } finally {
+    reader.releaseLock();
+  }
+}
+
+/**
+ * @param {Buffer} buf
+ * @param {string} hashAlgorithm 
+ * @returns {Promise<string>}
+ */
+async function getHash (buf, hashAlgorithm) {
+  const digest = await createDigest(hashAlgorithm, buf)
+  return digest.toString('hex')
+}
+
+// /**
+//  * @param {string} dest - file path
+//  * @param {string} hash - hash string
+//  * @param {string} hashAlgorithm - hash algorithm 
+//  * @returns {Promise<boolean>}
+//  */
+// export async function checkHash (dest, hash, hashAlgorithm) {
+//   let buf
+//   try {
+//     buf = await readFile(dest)
+//   } catch (err) {
+//     // download if file is corrupted or does not exist
+//     return false
+//   }
+//   return hash === await getHash(buf, hashAlgorithm)
+// }
+
+// /**
+//  * @param {string} url - url to download
+//  * @returns {Promise<Buffer>}
+//  * @throws {Error} - if status code is not 200
+//  */
+// export async function download (url) {
+//   const response = await fetch(url, { mode: 'cors' })
+//   if (!response.ok) {
+//     throw new Error(`Bootstrap request failed: ${response.status} ${response.statusText}`)
+//   }
+//   const contentLength = +response.headers.get('Content-Length')
+//   let receivedLength = 0
+//   let prevProgress = 0
+//   const fileData = new Uint8Array(contentLength)
+
+//   for await (const chunk of streamAsyncIterable(response.body)) {
+//     fileData.set(chunk, receivedLength)
+//     receivedLength += chunk.length
+//     const progress = (receivedLength / contentLength * 100) | 0
+//     if (progress !== prevProgress) {
+//       this.emit('download-progress', progress)
+//       prevProgress = progress
+//     }
+//   }
+//   return fileData
+// }
 
 class Bootstrap extends EventEmitter {
   #hashActual = null
@@ -54,11 +120,6 @@ class Bootstrap extends EventEmitter {
     }
   }
 
-  async getHash (buf, hashAlgorithm) {
-    const digest = await createDigest(hashAlgorithm, buf)
-    return digest.toString('hex')
-  }
-
   async checkHash (dest, hash, hashAlgorithm) {
     let buf
     try {
@@ -73,40 +134,26 @@ class Bootstrap extends EventEmitter {
   }
 
   async download (url) {
-    const response = await fetch(url, {
-      mode: 'cors'
-    })
+    const response = await fetch(url, { mode: 'cors' })
     if (!response.ok) {
       this.cleanup()
       throw new Error(`Bootstrap request failed: ${response.status} ${response.statusText}`)
     }
-    const reader = response.body.getReader()
     const contentLength = +response.headers.get('Content-Length')
     let receivedLength = 0
-    let chunks = []
     let prevProgress = 0
+    const fileData = new Uint8Array(contentLength)
 
-    const read = async () => {
-      const {done, value} = await reader.read();
-      if (done) return
-      chunks.push(value);
-      receivedLength += value.length;
+    for await (const chunk of streamAsyncIterable(response.body)) {
+      fileData.set(chunk, receivedLength)
+      receivedLength += chunk.length
       const progress = (receivedLength / contentLength * 100) | 0
       if (progress !== prevProgress) {
         this.emit('download-progress', progress)
         prevProgress = progress
       }
-      await read()
     }
-    await read()
-
-    const uint8data = new Uint8Array(receivedLength);
-    let position = 0;
-    for(let chunk of chunks) {
-      uint8data.set(chunk, position);
-      position += chunk.length;
-    }
-    return uint8data
+    return fileData
   }
 
   cleanup () {
@@ -119,4 +166,11 @@ export function bootstrap (options) {
   const bootstrap = new Bootstrap(options)
   bootstrap.run()
   return bootstrap
+}
+
+// TODO: move to WebWorker?
+export default {
+  // checkHash,
+  // download,
+  bootstrap
 }
