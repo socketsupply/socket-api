27f6f971513f89ac1863642ae32371d6d0e6ba1c
Joseph Werle <joseph.werle@gmail.com>
2022-10-21 15:00:59 +0200

    refactor(): clean up, fix typos, lint

--- platform.js
+++ platform.js
@@ -1,17 +1,25 @@
-import * as ipc from './ipc.js'
+/**
+ * @module IPC
+ */
+
+/* global window */
 import { applyPolyFills } from './polyfills.js'
+import { format } from './util.js'
+import ipc from './ipc.js'
 
+// eslint-disable-next-line
 export const args = new class Args {
-  arch = window.__args.arch
-  argv = window.__args.argv || []
-  debug = window.__args.debug || false
-  env = window.__args.env || {}
-  executable = window.__args.executable || null
-  index = window.__args.index || 0
-  port = window.__args.port || 0
-  title = window.__args.title || null
-  version = window.__args.version || null
+  arch = window?.__args?.arch
+  argv = window?.__args?.argv || []
+  debug = window?.__args?.debug || false
+  env = window?.__args?.env || {}
+  executable = window?.__args?.executable || null
+  index = window?.__args?.index || 0
+  port = window?.__args?.port || 0
+  title = window?.__args?.title || null
+  version = window?.__args?.version || null
 
+  // eslint-disable-next-line
   config = new class Config {
     get size () {
       return Object.keys(this).length
@@ -37,80 +45,91 @@ if (globalThis.window) {
   applyPolyFills(globalThis.window)
 }
 
-function send () {
+export async function send (options) {
   let value = ''
 
   try {
-    value = JSON.stringify(o)
+    value = JSON.stringify(options)
   } catch (err) {
     return Promise.reject(err.message)
   }
 
-  return ipc.send('send', { value })
+  return await ipc.send('send', { value })
 }
 
 function redirectOutput () {
-  const clog = console.log
-  const cerr = console.error
-
-  console.log = (...args) => {
-    clog(...args)
-    ipc.postMessage(`ipc://stdout?value=${args}`)
+  const mapping = {
+    stdout: ['info', 'log'],
+    stderr: ['debug', 'error', 'warn']
   }
 
-  console.error = (...args) => {
-    cerr(...args)
-    ipc.postMessage(`ipc://stderr?value=${args}`)
+  for (const name of mapping.stdout) {
+    const fn = console[name]
+    console[name] = (...args) => {
+      const value = encodeURIComponent(args.map((arg) => format(arg)).join(' '))
+      ipc.postMessage(`ipc://stdout?value=${value}`)
+      return fn.apply(console, args)
+    }
+  }
+
+  for (const name of mapping.stderr) {
+    const fn = console[name]
+    console[name] = (...args) => {
+      const value = encodeURIComponent(args.map((arg) => format(arg)).join(' '))
+      ipc.postMessage(`ipc://stderr?value=${value}`)
+      return fn.apply(console, args)
+    }
   }
 }
 
+// FIXME: this should be platform agnostic
 if (args.platform !== 'linux') {
   redirectOutput()
 }
 
-export const openExternal = o => {
-  ipc.postMessage(`ipc://external?value=${encodeURIComponent(o)}`)
+export async function openExternal (options) {
+  return await ipc.postMessage(`ipc://external?value=${encodeURIComponent(options)}`)
 }
 
-export function exit (o) {
-  return ipc.send('exit', o)
+export async function exit (o) {
+  return await ipc.send('exit', o)
 }
 
-export function setTitle (o) {
-  return ipc.send('title', o)
+export async function setTitle (o) {
+  return await ipc.send('title', o)
 }
 
-export function inspect (o) {
-  ipc.postMessage(`ipc://inspect`)
+export async function inspect (o) {
+  return await ipc.postMessage('ipc://inspect')
 }
 
-export function show (index = 0) {
-  return ipc.send('show', { index })
+export async function show (index = 0) {
+  return await ipc.send('show', { index })
 }
 
-export function hide = (index = 0) {
-  return ipc.send('hide', { index })
+export async function hide (index = 0) {
+  return await ipc.send('hide', { index })
 }
 
-export function setWindowBackgroundColor (opts) {
+export async function setWindowBackgroundColor (opts) {
   opts.index = window.__args.index
   const o = new URLSearchParams(opts).toString()
-  ipc.postMessage(`ipc://background?${o}`)
+  await ipc.postMessage(`ipc://background?${o}`)
 }
 
-export function setContextMenu (o) {
+export async function setContextMenu (o) {
   o = Object
     .entries(o)
     .flatMap(a => a.join(':'))
     .join('_')
-  return ipc.send('context', o)
+  return await ipc.send('context', o)
 }
 
-export function setSystemMenuItemEnabled (value) {
-  return ipc.send('systemMenuItemEnabled', value)
+export async function setSystemMenuItemEnabled (value) {
+  return await ipc.send('systemMenuItemEnabled', value)
 }
 
-export function setSystemMenu (o) {
+export async function setSystemMenu (o) {
   const menu = o.value
 
   // validate the menu
@@ -157,5 +176,9 @@ export function setSystemMenu (o) {
   }
 
   // send the request to set the menu
-  return ipc.send('menu', o)
+  return await ipc.send('menu', o)
 }
+
+// eslint-disable-next-line
+import * as exports from './platform.js'
+export default exports
