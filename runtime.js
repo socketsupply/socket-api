ca674dec69286936ee1160326fb8f69bf7984d87
heapwolf <paolo@socketsupply.co>
2022-10-21 18:09:12 +0200

    add docs

--- runtime.js
+++ runtime.js
@@ -131,6 +131,88 @@ export async function setSystemMenuItemEnabled (value) {
   return await ipc.send('systemMenuItemEnabled', value)
 }
 
+/*
+ * Set the native menu for the app.
+ *
+ * Socket Runtime provides a minimalist DSL that makes it easy to create
+ * cross platform native system and context menus.
+ *
+ * Menus are created at run time. They can be created from either the Main or
+ * Render process. The can be recreated instantly by calling the `setMenu` method.
+ *
+ * The method takes a string. Here's an example of a menu. The semi colon is
+ * significant indicates the end of the menu. Use an underscore when there is no
+ * accelerator key. Modifiers are optional. For the edit menu, `op` will figure
+ * out which accelerators to use for you.
+ *
+ *
+ * ```js
+ * system.setMenu({ index: 0, value: `
+ *   App:
+ *     Foo: f;
+ *
+ *   Edit:
+ *     Cut: x
+ *     Copy: c
+ *     Paste: v
+ *     Delete: _
+ *     Select All: a;
+ *
+ *   Other:
+ *     Apple: _
+ *     Another Test: T
+ *     !Im Disabled: I
+ *     Some Thing: S + Meta
+ *     ---
+ *     Bazz: s + Meta, Control, Alt;
+ * `)
+ * ```
+ *
+ * #### Separators
+ *
+ * To create a separator, use three dashes `---`.
+ *
+ * #### Accelerator Modifiers
+ *
+ * Accelerator modifiers are used as visual indicators but don't have a
+ * material impact as the actual key binding is done in the event listener.
+ *
+ * A capital letter implies that the accelerator is modified by the `Shift` key.
+ *
+ * Additional accelerators are `Meta`, `Control`, `Option`, each separated
+ * by commas. If one is not applicable for a platform, it will just be ignored.
+ *
+ * On MacOS `Meta` is the same as `Command`.
+ *
+ * #### Disabled Items
+ *
+ * If you want to disable a menu item just prefix the item with the `!` character.
+ * This will cause the item to appear disabled when the system menu renders.
+ *
+ * #### Submenus
+ *
+ * We feel like nested menus are an anti-pattern. We don't use them. If you have a
+ * strong argument for them and a very simple pull request that makes them work we
+ * may consider them.
+ *
+ * #### Event Handling
+ *
+ * When a menu item is activated, it raises the `menuItemSelected` event in
+ * the front end code, you can then communicate with your backend code if you
+ * want from there.
+ *
+ * For example, if the `Apple` item is selected from the `Other` menu...
+ *
+ * ```js
+ * window.addEventListener('menuItemSelected', event => {
+ * assert(event.detail.parent === 'Other')
+ * assert(event.detail.title === 'Apple')
+ * })
+ * ```
+ *
+ * @param {string} menu
+ * @return {Promise<Any>}
+ */
 export async function setSystemMenu (o) {
   const menu = o.value
 
