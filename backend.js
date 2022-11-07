d676bc25d6ddbf6c4edf99f723f560f7641a7b93
Sergey Rubanov <chi187@gmail.com>
2022-11-07 20:23:24 +0400

    add backend module

---
+++ backend.js
@@ -0,0 +1,20 @@
+/**
+ * @module Backend
+ *
+ * Provides methods for the backend process management
+ */
+
+ import ipc from './ipc.js'
+
+/**
+ * @param {object} opts - an options object
+ * @param {boolean} [opts.force = false] - whether to force existing process to close
+ * @return {Promise<ipc.Result>}
+ */
+ export async function open ({ force } = { force: false }) {
+  return await ipc.send('process.open', { force })
+}
+
+export async function close () {
+  return await ipc.send('process.kill')
+}
