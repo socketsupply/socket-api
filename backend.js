0a5c8c5c59ce0b9fc75f86126163d5cb8e8b5c00
Sergey Rubanov <chi187@gmail.com>
2022-11-15 17:26:42 +0400

    resolve to proper window

--- backend.js
+++ backend.js
@@ -5,19 +5,22 @@
  */
 
  import ipc from './ipc.js'
+ import { args } from './runtime.js'
 
 /**
  * @param {object} opts - an options object
  * @param {boolean} [opts.force = false] - whether to force existing process to close
  * @return {Promise<ipc.Result>}
  */
-export async function open ({ force } = { force: false }) {
-  return ipc.send('process.open', { force })
+export async function open (opts = {}) {
+  opts.index = args.index
+  opts.force ??= false
+  return ipc.send('process.open', opts)
 }
 
 /**
  * @return {Promise<ipc.Result>}
  */ 
 export async function close () {
-  return ipc.send('process.kill')
+  return ipc.send('process.kill', { index: args.index })
 }
