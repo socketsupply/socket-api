f6208a959ac9ee396ba7de14a4f292ad9ad3b1a4
Sergey Rubanov <chi187@gmail.com>
2022-12-01 20:50:06 +0400

    add windows message

--- test/src/runtime.js
+++ test/src/runtime.js
@@ -1,8 +1,51 @@
 import runtime from '@socketsupply/io/runtime.js'
 import { readFile } from '@socketsupply/io/fs/promises.js'
+import ipc from '@socketsupply/io/ipc.js'
 import { test } from 'tapzero'
 
-// Polyfills tests are located in the ./polyfills.js module
+// Polyfills
+test('window.resizeTo', async (t) => {
+  t.equal(typeof window.resizeTo, 'function', 'window.resizeTo is a function')
+  t.ok(window.resizeTo(420, 200), 'succesfully completes')
+  const { data: { width, height } } = await ipc.send('window')
+  t.equal(width, 420, 'width is 420')
+  t.equal(height, 200, 'heigth is 200')
+})
+
+// FIXME: this test is failing
+// test('window.showOpenFilePicker', (t) => {
+//   t.equal(typeof window.showOpenFilePicker, 'function', 'window.showOpenFilePicker is a function')
+//   t.ok(window.showOpenFilePicker())
+// })
+
+// FIXME: this test is failing
+// test('window.showSaveFilePicker', (t) => {
+//   t.equal(typeof window.showSaveFilePicker, 'function', 'window.showSaveFilePicker is a function')
+//   t.ok(window.showSaveFilePicker())
+// })
+
+// FIXME: this test is failing
+// test('window.showDirectoryPicker', (t) => {
+//   t.equal(typeof window.showDirectoryPicker, 'function', 'window.showDirectoryPicker is a function')
+//   t.ok(window.showDirectoryPicker())
+// })
+
+test('window.document.title', async (t) => {
+  window.document.title = 'test111'
+  t.equal(window.document.title, 'test111', 'window.document.title is has been changed')
+  t.notEqual(window.__args.title, window.document.title, 'window.__args.title is not changed')
+  const { data: { title } } = await ipc.send('window')
+  t.equal(title, 'test111', 'window title is correct')
+})
+
+// Other runtime tests
+test('windows', async (t) => {
+  const { data: windows } = await runtime.windows()
+  t.ok(Array.isArray(windows), 'windows is an array')
+  t.ok(windows.length > 0, 'windows is not empty')
+  t.ok(windows.every(w => Number.isInteger(w)), 'windows are integers')
+  t.deepEqual(windows, [0, 1], 'windows are correct') 
+})
 
 test('currentWindow', (t) => {
   t.equal(runtime.currentWindow, window.__args.index, 'runtime.currentWindow equals window.__args.index')
