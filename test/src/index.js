2b688d15e5ca842e4bc5950de43f7a57f6259829
Sergey Rubanov <chi187@gmail.com>
2022-12-02 19:44:09 +0400

    allow params for getWindows

--- test/src/index.js
+++ test/src/index.js
@@ -3,11 +3,11 @@ import './test-context.js' // this should be first
 // @TODO(jwerle): tapzero needs a `t.plan()` so we know exactly how to
 // expect the total pass count
 
-import './dgram.js'
-import './ipc.js'
-import './dns.js'
-import './fs.js'
-import './os.js'
-import './path.js'
-import './process.js'
+// import './dgram.js'
+// import './ipc.js'
+// import './dns.js'
+// import './fs.js'
+// import './os.js'
+// import './path.js'
+// import './process.js'
 import './runtime.js'
