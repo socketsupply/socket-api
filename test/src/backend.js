d02ed9a6e4f5aeb99c1e24f592c27830ac28abd8
Sergey Rubanov <chi187@gmail.com>
2022-12-08 16:35:19 +0400

    tests for backend

---
+++ test/src/backend.js
@@ -0,0 +1,33 @@
+import backend from '@socketsupply/io/backend.js'
+import { test } from 'tapzero'
+
+test('backend.open()', async (t) => {
+  const openResult = await backend.open()
+  t.deepEqual(Object.keys(openResult.data).sort(), ['argv', 'cmd', 'path'], 'returns a result with the correct keys')
+})
+
+test('backend', async (t) => {
+  const successOpenPromise = Promise.race([
+    new Promise(resolve => window.addEventListener('process-error', ({ detail }) => {
+      if (detail) t.fail(`process-error event emitted: ${detail}`)
+      resolve(false)
+    }, { once: true })),
+    new Promise(resolve => setTimeout(() => resolve(true), 0))
+  ])
+  const backendReadyPromise = new Promise(resolve => window.addEventListener('backend:ready', () => resolve(true), { once: true }))
+  const backenSendDataPromise = new Promise(resolve => window.addEventListener('character', ({ detail }) => resolve(detail), { once: true }))
+  const [successOpen, backendReady, backendSendData] = await Promise.all([successOpenPromise, backendReadyPromise, backenSendDataPromise])
+  t.ok(successOpen, 'does not emit a process-error event')
+  t.ok(backendReady, 'can send events to window 0')
+  t.deepEqual(backendSendData, { character: { firstname: 'Morty', secondname: 'Smith' }}, 'can send events with data')
+})
+
+test('backend.open() again', async (t) => {
+  const openResult = await backend.open()
+  t.deepEqual(Object.keys(openResult.data).sort(), ['argv', 'cmd', 'path'], 'returns a result with the correct keys')
+  const doesNotRestart = await Promise.race([
+    new Promise(resolve => window.addEventListener('backend:ready', () => resolve(false), { once: true })),
+    new Promise(resolve => setTimeout(() => resolve(true), 256))
+  ])
+  t.ok(doesNotRestart, 'does not emit a backend:ready event')
+})
