d2777332722cb10c1373e27f73ecc07a5f23297f
Joseph Werle <joseph.werle@gmail.com>
2022-12-19 22:18:15 +0100

    fix(test): try to determine emulator path

--- test/scripts/bootstrap-android-emulator.sh
+++ test/scripts/bootstrap-android-emulator.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+declare ANDROID_HOME="${ANDROID_HOME:-$HOME/.android/sdk}"
+
 declare emulator_flags=()
+declare emulator="$(which emulator 2>/dev/null)"
+
+if [ -z "$emulator" ]; then
+  emulator="$ANDROID_HOME/emulator/emulator"
+fi
 
 emulator_flags+=(
   -gpu swiftshader_indirect
