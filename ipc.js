434870ae4c9fb9bf518d5c9c356ce7062fb03d2c
Joseph Werle <joseph.werle@gmail.com>
2022-12-23 20:59:48 +0100

    efactor(ipc.js): improve win32 XHR ipc requests with 'http:'

--- ipc.js
+++ ipc.js
@@ -211,7 +211,7 @@ function getRequestResponse (request) {
     }
 
     const { status, responseURL, statusText } = request
-    const message = Message.from(responseURL)
+    const message = Message.from(responseURL?.replace('http:', Message.PROTOCOL))
     const source = message.command
 
     if (status >= 100 && status < 400) {
@@ -299,6 +299,14 @@ function maybeMakeError (error, caller) {
   return err
 }
 
+function createUri (protocol, command) {
+  if (typeof window === 'object' && window?.__args.os === 'win32') {
+    protocol = 'http:'
+  }
+
+  return `${protocol}//${command}`
+}
+
 /**
  * Represents an OK IPC status.
  */
@@ -372,7 +380,7 @@ export class Message extends URL {
    * The expected protocol for an IPC message.
    */
   static get PROTOCOL () {
-    return window?.__args?.os === 'win32' ? 'http:' : 'ipc:'
+    return 'ipc:'
   }
 
   /**
@@ -757,7 +765,7 @@ export function sendSync (command, params) {
   const request = new window.XMLHttpRequest()
   const index = window.__args.index ?? 0
   const seq = nextSeq++
-  const uri = `${protocol}//${command}`
+  const uri = createUri(protocol, command)
 
   params = new URLSearchParams(params)
   params.set('index', index)
@@ -908,7 +916,7 @@ export async function write (command, params, buffer, options) {
   const signal = options?.signal
   const index = window.__args.index ?? 0
   const seq = nextSeq++
-  const uri = `${protocol}//${command}`
+  const uri = createUri(protocol, command)
 
   let resolved = false
   let aborted = false
@@ -1000,7 +1008,7 @@ export async function request (command, params, options) {
   const signal = options?.signal
   const index = window.__args.index ?? 0
   const seq = nextSeq++
-  const uri = `${protocol}//${command}`
+  const uri = createUri(protocol, command)
 
   let resolved = false
   let aborted = false
