60b47a01e43a8fcf445e6cefcd6a63614f0ffbff
Sergey Rubanov <chi187@gmail.com>
2022-12-26 17:16:42 +0400

    path: fix win delimiter

--- path/win32.js
+++ path/win32.js
@@ -2,5 +2,5 @@ import { Path } from './path.js'
 
 export default class Win32Path extends Path {
   static get sep () { return '\\' }
-  static get delimiter () { return ':' }
+  static get delimiter () { return ';' }
 }
--- test/src/path.js
+++ test/src/path.js
@@ -4,9 +4,10 @@ import os from '../../os.js'
 import { test } from '@socketsupply/tapzero'
 
 test('path', (t) => {
-  t.ok(path, 'path exports')
   t.ok(path.posix, 'path.posix exports')
   t.ok(path.win32, 'path.win32 exports')
   const expectedSep = os.platform() === 'win32' ? '\\' : '/'
   t.equal(path.sep, expectedSep, 'path.sep is correct')
+  const expectedDelimiter = os.platform() === 'win32' ? ';' : ':'
+  t.equal(path.delimiter, expectedDelimiter, 'path.delimiter is correct')
 })
