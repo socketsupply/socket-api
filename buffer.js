bfc91358ded0ea9c5aaa97ccd6d79ef12cacb3d2
Joseph Werle <joseph.werle@gmail.com>
2023-01-09 20:09:12 +0100

    fix(buffer.js): use 'import *'

--- buffer.js
+++ buffer.js
@@ -4,7 +4,7 @@
  * @see {@link https://nodejs.org/api/buffer.html#buffer_class_buffer}
  */
 
-import buffer from 'buffer'
+import * as buffer from 'buffer'
 export const Buffer = buffer.Buffer ?? buffer
 export * from 'buffer'
 export default Buffer
