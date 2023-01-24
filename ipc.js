fd0f06e4cba44edc81c7d45cd98e61e9f2476af3
Trevor Norris <trev.norris@gmail.com>
2023-01-24 15:11:20 -0700

    Revert "fix(ipc.js): check for 'win32', not 'win'"
    This reverts commit 38d537c628f0bb70a49ff83c80e6dc0501289ddc.

--- ipc.js
+++ ipc.js
@@ -377,7 +377,7 @@ export class Message extends URL {
    * The expected protocol for an IPC message.
    */
   static get PROTOCOL () {
-    return window?.__args?.os === 'win32' ? 'http:' : 'ipc:'
+    return window?.__args?.os === 'win' ? 'http:' : 'ipc:'
   }
 
   /**
